baeec247bcce44cbd19c1b1e580a60d76841b994
Your Name <your@email.com>
2026-03-19 21:43:04 +0530

    fixed app for deployment

--- app.py
+++ app.py
@@ -226,4 +226,7 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
